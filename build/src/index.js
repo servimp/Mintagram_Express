921b9838877763784c13bf7b49342ce50464df54
Manuel Rueda <luis.rueda@bizglo.com>
2023-03-03 16:49:18 -0500

    Update vercel.json file

--- build/src/index.js
+++ build/src/index.js
@@ -100,3 +100,6 @@ data_source_1.AppDataSource.initialize().then(function () { return __awaiter(voi
     });
 }); }).catch(function (error) { return console.log(error); });
 //# sourceMappingURL=index.js.map
+
+// Export the Express API
+module.exports = app;
