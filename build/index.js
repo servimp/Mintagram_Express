58d376f6646bd8b5a121433003e3479f8f733387
Manuel Rueda <luis.rueda@bizglo.com>
2023-03-04 08:37:48 -0500

    Revised export default on index.js

--- build/index.js
+++ build/index.js
@@ -92,10 +92,9 @@ data_source_1.AppDataSource.initialize().then(function () { return __awaiter(voi
         console.log("Express server has started on port 5000. Open http://localhost:5000/users to see results");
         // start express server
         app.listen(5000);
-        return [2 /*return*/];
+        // Export the Express API
+        module.exports = app;
     });
 }); }).catch(function (error) { return console.log(error); });
 //# sourceMappingURL=index.js.map
 
-// Export the Express API
-module.exports = app;
