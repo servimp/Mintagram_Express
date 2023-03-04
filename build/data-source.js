6cd182419befeca3fc8095e5a31a34df36053455
Manuel Rueda <luis.rueda@bizglo.com>
2023-03-04 10:35:35 -0500

    Added Postgres settings for Railway

--- build/data-source.js
+++ build/data-source.js
@@ -7,11 +7,11 @@ var User_1 = require("./entity/User");
 var Post_1 = require("./entity/Post");
 exports.AppDataSource = new typeorm_1.DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
+    host: "containers-us-west-96.railway.app",
+    port: 6454,
     username: "postgres",
-    password: "MtDbPass",
-    database: "postgres",
+    password: "nsl6w0CIuLAFnEor6Tei",
+    database: "railway",
     synchronize: true,
     logging: false,
     entities: [Post_1.Post, User_1.User],
